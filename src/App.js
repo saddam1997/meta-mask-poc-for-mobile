42c7df766ccf6c0be9f9e76acb90d6ab79c2f14c
saddam1997 <saddam.husen1997@gmail.com>
2024-10-01 17:19:08 +0530

    asdf

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
 
         <a href="https://metamask.app.link/send/0x4081444763A8d6E2f1476C4357b6E46292Fa5feC">Send ETH with MetaMask</a>
 
